0285b29bf03cc404868796cc41ed897fdf0d9d67
suigarra <suigarra@puffy.Home>
2025-08-04 16:38:30 -0300

    First commit

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use ftp_client::prelude::*;
+use std::io;
+
+fn main() -> Result<(), ftp_client::error::Error>  {
+    let mirror = "openbsd.cs.toronto.edu";
+
+    let mut client = Client::connect(mirror, "anonymous", "anonymous")?;
+   
+    let _ = client.cwd("pub/OpenBSD/7.7/packages/amd64");
+    println!("{:?}", client.pwd());
+
+    loop {
+        println!();
+        println!("Search for package: ");
+        let mut keyword = String::new();
+        io::stdin().read_line(&mut keyword).unwrap();
+        let keyword = keyword.trim();
+
+        if keyword.eq_ignore_ascii_case("quit") {
+            break;
+        }
+
+        if keyword.is_empty() {
+            continue;
+        }
+
+        let names = client.list_names(".")?;
+        println!("Listing packages with the '{}' keyword:", keyword);
+        println!();
+        for name in names {
+            if name.to_lowercase().contains(&keyword.to_lowercase()) {
+                let n = name.replace(".tgz", "");
+                println!("{}", n);
+            }
+        }
+    }
+
+    Ok(())
+}
